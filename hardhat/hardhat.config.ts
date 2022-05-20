7e1a73e5c26b5201a058d3854618034be54faeec
Ayton MacEachern <8theVirulent@gmail.com>
2022-05-19 19:43:15 -0600

    add etherscan api

--- hardhat/hardhat.config.ts
+++ hardhat/hardhat.config.ts
@@ -1,40 +1,45 @@
-import * as dotenv from 'dotenv';
+import * as dotenv from "dotenv";
 
-import { HardhatUserConfig } from 'hardhat/config';
-import '@nomiclabs/hardhat-etherscan';
-import '@nomiclabs/hardhat-waffle';
-import '@typechain/hardhat';
-require('solidity-coverage');
+import { HardhatUserConfig } from "hardhat/config";
+import "@nomiclabs/hardhat-etherscan";
+import "@nomiclabs/hardhat-waffle";
+import "@typechain/hardhat";
+require("solidity-coverage");
 
 dotenv.config();
 
 const config: HardhatUserConfig = {
-	solidity: {
-		compilers: [
-			{
-				version: '0.6.5',
-			},
-			{
-				version: '0.6.0',
-				settings: {},
-			},
-		],
-	},
-	networks: {
-		hardhat: {
-			gas: 'auto',
-		},
-		iExecSidechain: {
-			url: 'https://viviani.iex.ec',
-			accounts: process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
-		},
-		goerli: {
-			url: process.env.GOERLI_URL || '',
-			accounts: process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
-			gas: 10000000,
-			gasPrice: 10000000000,
-		},
-	},
+  solidity: {
+    compilers: [
+      {
+        version: "0.6.5",
+      },
+      {
+        version: "0.6.0",
+        settings: {},
+      },
+    ],
+  },
+  networks: {
+    hardhat: {
+      gas: "auto",
+    },
+    iExecSidechain: {
+      url: "https://viviani.iex.ec",
+      accounts:
+        process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+    },
+    goerli: {
+      url: process.env.GOERLI_URL || "",
+      accounts:
+        process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+      gas: 10000000,
+      gasPrice: 10000000000,
+    },
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY,
+  },
 };
 
 export default config;
